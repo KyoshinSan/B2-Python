ecc34b74e2d41c35a9a66fc0526a87b98cd32dd5
root <root@localhost.localdomain>
2018-11-05 23:25:19 +0100

    Ajout 2a-mol

---
+++ scripts/2a-mol/2a-mol.py
@@ -0,0 +1,80 @@
+#!/bin/python36
+#
+# Nom : 2a-mol.py
+# Description : Plus ou moins avec lecture dans un fichier
+# Date : 23/10/2018
+# Auteur : Jonathan DINH
+
+#IMPORTS
+import random
+import time
+import sys
+import signal
+
+#VARIABLES
+nombreAleatoire = random.randint(0,100)
+welcomeMsg = "Bienvenue au jeu du plus ou moins ! Trouvez un nombre compris entre 0 et 100. Vous devez ecrire dans fichier un nombre !"
+moreMsg = "C'est plus grand !"
+lessMsg = "C'est plus petit !"
+erreurMsg = "Veuillez ecrire un nombre entre 0 et 100 !"
+victoryMsg = 'Bravo, tu as gagne. La solution etait ' + str(nombreAleatoire)
+
+#FONCTIONS
+def youcant(sig, frame):
+	goodbye()
+	sys.exit(0)
+
+def EcrireMsgBienvenue():
+	fichier = open("nombre.txt", "w")
+	fichier.write(welcomeMsg)
+	fichier.close()
+
+def lectureNombre():
+	fichier = open("nombre.txt", "r")
+	return fichier.read()
+	fichier.close()
+
+def EcrirePlusPetit():
+	fichier = open("nombre.txt", "w")
+	fichier.write(lessMsg)
+	fichier.close()
+
+def EcrirePlusGrand():
+	fichier = open("nombre.txt", "w")
+	fichier.write(moreMsg)
+	fichier.close()
+
+def EcrireVictoire():
+	fichier = open("nombre.txt", "w")
+	fichier.write(victoryMsg)
+	fichier.close()
+
+def EcrireErreur():
+	fichier = open("nombre.txt", "w")
+	fichier.write(erreurMsg)
+	fichier.close()
+
+def goodbye():
+	print('\nLa solution etait ' + str(nombreAleatoire))
+	print('***Goodbye !***')
+
+#SCRIPT
+signal.signal(signal.SIGINT, youcant)
+signal.signal(signal.SIGTERM, youcant)
+EcrireMsgBienvenue()
+
+while True:
+	saisie = lectureNombre()
+	try:
+		saisie = int(saisie)
+		if saisie == nombreAleatoire:
+			EcrireVictoire()
+			break
+		elif saisie > nombreAleatoire:
+			EcrirePlusPetit()
+			time.sleep(1)
+		else:
+			EcrirePlusGrand()
+			time.sleep(1)
+	except ValueError:
+		time.sleep(1)
